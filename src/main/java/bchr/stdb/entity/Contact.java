c411f9ad6ffdcdbbd47f691aff04b1e11541cb85
Nolram91 <56675795+Nolram91@users.noreply.github.com>
2019-11-24 12:31:19 +0100

    Beifügen von neuen Spalten: - Telefon - Straße - Ort - PLZ - Land - Homepage - Gruppe - UIDNr - Inaktiv
    in Trello steht ja, dass die Klassen ContactDao und ContactController ebenso betroffen von den Änderungen sein sollten. Habe ich etwas vergessen? - Sorry Brandl, die TV- Aufzeichnung wurde nicht gespeichert D:
    
    Bitte um kleines Feedback

--- src/main/java/bchr/stdb/entity/Contact.java
+++ src/main/java/bchr/stdb/entity/Contact.java
@@ -23,6 +23,30 @@ public class Contact {
     @Column(name = "PHONE", length = 30)
     private String phoneNr;
 
+    @Column(name = "STREET", length = 60)
+    private String street;
+
+    @Column(name = "PLACE", length = 60)
+    private String place;
+
+    @Column(name = "POSTCODE", length = 10)
+    private String postcode;
+
+    @Column(name = "COUNTRY", length = 60)
+    private String country;
+
+    @Column(name = "HOMEPAGE", length = 100)
+    private String homepage;
+
+    @Column(name = "GROUP", length = 30)
+    private Integer contactGrp;
+
+    @Column(name = "UIDNr", length = 30)
+    private String uidNr;
+
+    @Column(name = "INACTIVE", nullable = false)
+    private boolean inactive;
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
@@ -46,17 +70,93 @@ public class Contact {
         this.email = email;
     }
 
-    public String getPhoneNr(){return phoneNr;}
+    public String getPhoneNr() {
+        return phoneNr;
+    }
 
-    public void setPhoneNr(String phoneNr){this.phoneNr = phoneNr;}
+    public void setPhoneNr(String phoneNr) {
+        this.phoneNr = phoneNr;
+    }
+
+    public Integer getContactGrp() {
+        return contactGrp;
+    }
+
+    public void setContactGrp(Integer contactGrp) {
+        this.contactGrp = contactGrp;
+    }
+
+    public String getUidNr() {
+        return uidNr;
+    }
+
+    public void setUidNr(String uidNr) {
+        this.uidNr = uidNr;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    public boolean isInactive() {
+        return inactive;
+    }
+
+    public void setInactive(boolean inactive) {
+        this.inactive = inactive;
+    }
 
     public Contact() {
     }
 
-    public Contact(String lastName, String firstName, String email, String phoneNr) {
+    public Contact(String lastName, String firstName, String email, String phoneNr, Integer contactGrp, String uidNr, String country, String place, String street, String postcode, String homepage ) {
         this.lastName=lastName;
         this.firstName=firstName;
         this.email=email;
         this.phoneNr=phoneNr;
+        this.contactGrp = contactGrp;
+        this.uidNr = uidNr;
+        this.country = country;
+        this.place = place;
+        this.street = street;
+        this.postcode = postcode;
+        this.homepage = homepage;
+        this.inactive = false;
     }
 }
