960d2dcae753dae716c815593d0c7ac163974950
Bchr <brandl.christian@hotmail.de>
2020-01-15 08:20:30 +0100

    additional Props for Account

--- src/main/java/bchr/stdb/entity/Account.java
+++ src/main/java/bchr/stdb/entity/Account.java
@@ -23,8 +23,106 @@ public class Account extends Auditable {
     @Column(name = "EMAIL", length = 70)
     private String email;
 
+    @Column(name = "PHONE", length = 30)
+    private String phone;
+
+    @Column(name = "PHONE2", length = 30)
+    private String phone2;
+
+    @Column(name = "STREET", length = 30)
+    private String street;
+
+    @Column(name = "PLACE", length = 70)
+    private String place;
+
+    @Column(name = "POSTCODE", length = 10)
+    private String postCode;
+
+    @Column(name = "COUNTRY", length = 30)
+    private String country;
+
+    @Column(name = "UST_ID", length = 40)
+    private String ustID;
+
+    @Column(name = "CONTACT_PERSON", length = 20)
+    private Integer contactID;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone2() {
+        return phone2;
+    }
+
+    public void setPhone2(String phone2) {
+        this.phone2 = phone2;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getUstID() {
+        return ustID;
+    }
+
+    public void setUstID(String ustID) {
+        this.ustID = ustID;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    public void setHomepage(String homepage) {
+        this.homepage = homepage;
+    }
+
+    public void setContacts(Set<bchr.stdb.entity.Contact> contacts) {
+        this.contacts = contacts;
+    }
+
+    @Column(name = "HOMEPAGE", length = 70)
+    private String homepage;
+
+
     @Column(name = "ACTIVE")
     private Boolean active;
+
+    @Column()
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "account")
     @JsonIgnoreProperties("account")
