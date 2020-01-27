a1ad30b175becdb1f78712c47227dc83c8cca9ee
Bchr <brandl.christian@hotmail.de>
2020-01-27 21:48:50 +0100

    After Demo

--- src/main/java/bchr/stdb/entity/Account.java
+++ src/main/java/bchr/stdb/entity/Account.java
@@ -50,7 +50,7 @@ public class Account extends Auditable {
 
     @ManyToOne
     @JoinColumn(name="GROUP_ID")
-    @JsonIgnoreProperties({"contacts", "accounts", "groups"})
+    @JsonIgnoreProperties(value = {"contacts", "accounts", "groups"})
     private Group branche;
 
     @Column(name = "HOMEPAGE", length = 70)
@@ -76,7 +76,7 @@ public class Account extends Auditable {
 
     @Column(name= "CONTACTS")
     @OneToMany(mappedBy = "account1")
-    @JsonIgnoreProperties(value = {"account1", "account2" , "account3"})
+    @JsonIgnoreProperties(value = {"account1", "account2" , "account3"}, allowSetters = true)
     private Set<Contact> contacts = new HashSet<Contact>();
 
     public Account() {
